80a0a77ad7239faf946f3c9fc4c442bf0d4c3de2
Leandro Pinzon Filho <131808539+Lfijho@users.noreply.github.com>
2024-09-02 09:30:36 -0300

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,15 @@
+//Escreva um algoritmo que imprima os números de 100 a 200 de 10 em 10.
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int num = 100;
+    for ( int i = 0; i <10; i++) {
+        printf("%d\n", num);
+        num = num + 10;
+    }
+
+
+}
