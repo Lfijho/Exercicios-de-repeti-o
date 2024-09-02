5e80c658499e13b4fae42f1cb5286398e38d3f88
Leandro Pinzon Filho <131808539+Lfijho@users.noreply.github.com>
2024-09-02 09:14:37 -0300

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,10 @@
+//Faça um programa que apresente os quadrados dos números inteiros de 15 a 200
+
+
+#include <stdio.h>
+
+int main(void) {
+  for ( int num = 15; num <= 200; num++) {
+      printf("%d\n", num * num ); 
+  }
+}
